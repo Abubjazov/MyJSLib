21265ad5ea4a9ceb346b28e0c1be6e6d11d4f3e9
Radik Abubjazov <abubjazov@mail.ru>
2021-08-06 22:56:04 +0300

    добавление методов в функцию

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -1,7 +1,23 @@
 (() => {
     const $ = function(selector) {
         const elements = document.querySelectorAll(selector);
-        console.log(elements);
+        const object = {};
+
+        object.hide = () => {
+            elements.forEach(elem => {
+                elem.style.display = 'none';
+            });
+            return object;
+        };
+
+        object.show = () => {
+            elements.forEach(elem => {
+                elem.style.display = '';
+            });
+            return object;
+        };
+
+        return object;
     };
 
     window.$ = $;
