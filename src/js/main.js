9924cae9209c71b1a5eea34f9f056adf38e79200
Radik Abubjazov <abubjazov@mail.ru>
2021-09-06 20:42:07 +0300

    добавлен модуль animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 import './lib/lib';
 
-$('button').click(function() {
-    $('div').eq(1).toggleClass('active');
-});
+// $('button').click(function() {
+//     $('div').eq(1).toggleClass('active');
+// });
 
-$('div').click(function() {
-    console.log($(this).index());
-});
+// $('div').click(function() {
+//     console.log($(this).index());
+// });
 
 // console.log($('div').eq(2).find('.some'));
 // console.log($('.some').closest('.findmew'));
-console.log($('.findme').siblings());
+// console.log($('.findme').siblings());
 
