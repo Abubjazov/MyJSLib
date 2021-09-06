9f139ce4246fd36fc0af2bbcc4fda570b660a14e
Radik Abubjazov <abubjazov@mail.ru>
2021-09-06 20:56:45 +0300

    модуль animations: + метод animateOverTime

--- src/js/lib/modules/animations.js
+++ src/js/lib/modules/animations.js
@@ -1,2 +1,26 @@
 import $ from '../core';
 
+$.prototype.animateOverTime = function(duration, callback, fin) {
+    let timeStart;
+
+    function _animateOverTime(time) {
+        if (!timeStart) {
+            timeStart = time;
+        }
+
+        let timeElapsed = time - timeStart;
+        let complection = Math.min(timeElapsed / duration, 1);
+
+        callback(complection);
+
+        if (timeElapsed < duration) {
+            requestAnimationFrame(_animateOverTime);
+        } else {
+            if (typeof fin === 'function') {
+                fin();
+            }
+        }
+    }
+
+    return _animateOverTime;
+};
