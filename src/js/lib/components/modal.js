7b8d14fc9319ec248a82c0a32e9f361db8ece5ba
Radik Abubjazov <abubjazov@mail.ru>
2021-09-07 22:27:02 +0300

    модуль modal: + динамическое создание окон

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -16,6 +16,10 @@ $.prototype.modal = function() {
         $(element).click(() => {
             $('.modal').fadeOut(500);
             document.body.style.overflow = '';
+            const modal = document.querySelector('.modal-js');
+            if (modal) {                
+                document.body.removeChild(modal);
+            }
         });
     });
 
@@ -23,8 +27,68 @@ $.prototype.modal = function() {
         if (e.target.classList.contains('modal')) {
             $('.modal').fadeOut(500);
             document.body.style.overflow = '';
+            const modal = document.querySelector('.modal-js');
+            if (modal) {                
+                document.body.removeChild(modal);
+            }
         }
     });
 };
 
 $('[data-toggle="modal"]').modal();
+
+$.prototype.createModal = function({text, btns} = {}) {
+    for (let i = 0; i <this.length; i++) {
+        let modal = document.createElement('div');
+        modal.classList.add('modal', 'modal-js');
+        modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+
+        const buttons = [];
+
+        for (let j = 0; j < btns.count; j++) {
+            let btn = document.createElement('button');
+            btn.textContent = btns.settings[j][0];
+            btn.classList.add('btn', ...btns.settings[j][1]);
+            if (btns.settings[j][2]) {
+                btn.setAttribute('modal-close', 'true');
+            }
+            if (btns.settings[j][3] && typeof(btns.settings[j][3]) === 'function') {
+                btn.addEventListener('click', btns.settings[j][3]);
+            }
+
+            buttons.push(btn);
+        }
+
+        modal.innerHTML = `
+        <div class="modal" id="exampleModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+
+                    <button class="close" modal-close>
+                        <span>&times;</span>
+                    </button>
+
+                    <div class="modal-header">
+                        <div class="modal-title">
+                            ${text.title}
+                        </div>
+                    </div>
+
+                    <div class="modal-body">
+                        ${text.body}
+                    </div>
+
+                    <div class="modal-footer">
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `;
+        modal.querySelector('.modal-footer').append(...buttons);
+        document.body.appendChild(modal);
+        $(this[i]).modal();
+        $(this[i].getAttribute('data-target')).fadeIn(300);
+    }
+};
--- src/js/main.js
+++ src/js/main.js
@@ -12,17 +12,50 @@ import './lib/lib';
 //     $('.w-500').fadeToggle(1000);
 // });
 
-$('.wrap').html(
-    `
-    <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
-        <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
-            <a href="#" class="dropdown-item">Action#1</a>
-            <a href="#" class="dropdown-item">Action#2</a>
-            <a href="#" class="dropdown-item">Action#3</a>
-        </div>
-    </div>
-    `
-);
+// $('.wrap').html(
+//     `
+//     <div class="dropdown">
+//         <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+//         <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+//             <a href="#" class="dropdown-item">Action#1</a>
+//             <a href="#" class="dropdown-item">Action#2</a>
+//             <a href="#" class="dropdown-item">Action#3</a>
+//         </div>
+//     </div>
+//     `
+// );
 
-$('.dropdown-toggle').dropdown();
+// $('.dropdown-toggle').dropdown();
+
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'JS Modal Title',
+        body: '13 Lorem ipsum dolor, sit amet consectetur adipisicing elit! Doloribus.'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn', 'btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Success',
+                ['btn', 'btn-success', 'mr-10'],
+                false,
+                () => {
+                    alert('Данные отправлены на сервер');
+                }
+            ],
+            [
+                'Warning',
+                ['btn', 'btn-warning'],
+                false,
+                () => {
+                    alert('Чувак это success!!!!');
+                }
+            ]
+        ]
+    }
+}));
