fc9c60adeb629ea10b991b635c02c14814e404fc
Radik Abubjazov <abubjazov@mail.ru>
2021-09-07 18:41:55 +0300

    добавлен модуль dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,28 @@
 import './lib/lib';
 
-$('#first').triggerOn('click', () => {
-    $('div').eq(1).fadeToggle(1000);
-});
+// $('#first').triggerOn('click', () => {
+//     $('div').eq(1).fadeToggle(1000);
+// });
 
-$('[data-count="second"]').triggerOn('click', () => {
-    $('div').eq(2).fadeToggle(1000);
-});
+// $('[data-count="second"]').triggerOn('click', () => {
+//     $('div').eq(2).fadeToggle(1000);
+// });
 
-$('button').eq(2).triggerOn('click', () => {
-    $('.w-500').fadeToggle(1000);
-});
+// $('button').eq(2).triggerOn('click', () => {
+//     $('.w-500').fadeToggle(1000);
+// });
+
+$('.wrap').html(
+    `
+    <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+        <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+            <a href="#" class="dropdown-item">Action#1</a>
+            <a href="#" class="dropdown-item">Action#2</a>
+            <a href="#" class="dropdown-item">Action#3</a>
+        </div>
+    </div>
+    `
+);
+
+$('.dropdown-toggle').dropdown();
